8c7cdc182f69bbe891a68316defd45f141566b71
LaughingLove <jokelovehate@gmail.com>
2018-08-10 17:23:35 -0400

    Added vouches page

---
+++ js/vouches.js
@@ -0,0 +1,14 @@
+// Thanatos IMG
+var viewer = new Viewer(document.getElementById("thanatos"), {
+    inline: false,
+    navbar: 2,
+    toolbar: 2,
+    loading: true
+});
+// Oinky IMG
+var viewer2 = new Viewer(document.getElementById("oinky"), {
+    inline: false,
+    navbar: 2,
+    toolbar: 2,
+    loading: true
+});
